8d23c80853935cc68e51ba0af19fe8e2ea75e3fc
Fabio Sarcinelli <fabiosarci125@gmail.com>
2025-01-02 20:19:23 +0100

    UserQuizService creation

---
+++ src/main/java/com/infobasic/sviluppo_sowftare/service/UserQuizService.java
@@ -0,0 +1,31 @@
+package com.infobasic.sviluppo_sowftare.service;
+
+import com.infobasic.sviluppo_sowftare.dao.UserQuizDao;
+import com.infobasic.sviluppo_sowftare.model.UserQuiz;
+
+import java.util.List;
+
+public class UserQuizService {
+
+    private static UserQuizDao userQuizDao = new UserQuizDao();
+
+    public UserQuiz addUserQuiz(UserQuiz userQuiz){
+        return userQuizDao.create(userQuiz);
+    }
+
+    public UserQuiz editUserQuiz(UserQuiz userQuiz){
+        return userQuizDao.create(userQuiz);
+    }
+
+    public UserQuiz findUserQuizById(int id){
+        return userQuizDao.findById(id);
+    }
+
+    public List<UserQuiz> findAllUserQuiz(){
+        return userQuizDao.findAll();
+    }
+
+    public void deleteUserQuizById(int id){
+        userQuizDao.deleteById(id);
+    }
+}
