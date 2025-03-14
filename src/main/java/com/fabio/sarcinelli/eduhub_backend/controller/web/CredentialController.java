6888f2ca33f7612e0c4d8765a9b12da6b6b47657
Fabio Sarcinelli <fabiosarci125@gmail.com>
2025-03-14 17:14:52 +0100

    creation of all credentialController apis

--- src/main/java/com/fabio/sarcinelli/eduhub_backend/controller/web/CredentialController.java
+++ src/main/java/com/fabio/sarcinelli/eduhub_backend/controller/web/CredentialController.java
@@ -5,9 +5,11 @@ import com.fabio.sarcinelli.eduhub_backend.service.CredentialService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,10 +44,60 @@ public class CredentialController {
         }
     }
 
+    @GetMapping
+    public ResponseEntity<Iterable<Credential>> findAll(){
+
+        Iterable<Credential> credentials = credentialService.findAll();
+        return ResponseEntity.ok(credentials);
+    }
+
+    @GetMapping("/exist-username/{username}")
+    public ResponseEntity<Boolean> existByUsername(@PathVariable String username){
+
+        Boolean exist = credentialService.existsByEmail(username);
+        if(exist){
+            return ResponseEntity.ok(exist);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/exist-email/{email}")
+    public ResponseEntity<Boolean> existByEmail(@PathVariable String email){
+
+        Boolean exist = credentialService.existsByEmail(email);
+        if(exist){
+            return ResponseEntity.ok(exist);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
     @PostMapping
     public ResponseEntity<Credential> save(@RequestBody Credential credential){
 
         Credential credentialSaved = credentialService.save(credential);
         return ResponseEntity.ok(credentialSaved);
+    }
+
+    @PutMapping
+    public ResponseEntity<Credential> update(@RequestBody Credential credential){
+
+        Credential credentialUpdated = credentialService.update(credential);
+        return ResponseEntity.ok(credentialUpdated);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> delete(@RequestBody Credential credential){
+
+        credentialService.delete(credential);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id){
+
+        credentialService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
